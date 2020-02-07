f5161393de2d7e4c33d1a6ee31f59ee7902735e6
Jen <lopzjcan@gmail.com>
2020-02-07 13:27:36 -0500

    pop up panel, social media icons

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,2 +1,2 @@
-# Contributers
+# Contributors
 
---
+++ popup/study_buddy.js
@@ -0,0 +1,7 @@
+document.getElementById("plus").addEventListener("click", function() {
+    document.getElementById("other-site").style.display = "block";
+})
+
+document.getElementById("add").addEventListener("click", function() {
+    document.getElementById("other-site").style.display = "none";
+})
