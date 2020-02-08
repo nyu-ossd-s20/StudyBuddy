0b0725bf5110b6d884239370d30f09b5883ac6e3
Jen <lopzjcan@gmail.com>
2020-02-07 19:22:46 -0500

    updated popup UI

--- popup/study_buddy.js
+++ popup/study_buddy.js
@@ -1,7 +1,35 @@
-document.getElementById("plus").addEventListener("click", function() {
-    document.getElementById("other-site").style.display = "block";
+var addPicButton = document.getElementById("plus");
+var otherField = document.getElementById("other-site");
+
+var addButton = document.getElementById("add-site");
+var studyButton = document.getElementById("study");
+
+// check if the user clicked on the "add" Image button
+addPicButton.addEventListener("click",function() {
+    if (otherField.classList.contains("show")){
+        hide();
+    }
+    else{
+        show();
+    }
+}) 
+
+addButton.addEventListener("click", function(e){
+    e.preventDefault();
+    // TODO: validate input url 
+    hide();
 })
 
-document.getElementById("add").addEventListener("click", function() {
-    document.getElementById("other-site").style.display = "none";
-})
+// hides the "new site" input field
+function hide() {
+    otherField.classList.remove("show");
+    otherField.classList.add("hide");
+    addPicButton.src = "../socials/add.png";
+}
+
+// shows the "new site" input field
+function show() {
+    otherField.classList.remove("hide");
+    otherField.classList.add("show");
+    addPicButton.src = "../socials/minus.png";
+}
