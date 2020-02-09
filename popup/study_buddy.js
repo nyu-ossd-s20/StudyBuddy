467b78131e52c66a009b6e7298848204bfcf5473
Anthony Zeng <anthonyzeng99@gmail.com>
2020-02-09 18:01:17 -0500

    worked on storing blocked sites

--- popup/study_buddy.js
+++ popup/study_buddy.js
@@ -4,6 +4,20 @@ var otherField = document.getElementById("other-site");
 var addButton = document.getElementById("add-site");
 var studyButton = document.getElementById("study");
 
+const socialInputs = document.getElementsByName('social');
+
+const siteMappings = {
+    "fb" : "facebook.com",
+    "ig" : "instagram.com",
+    "rd" : "reddit.com",
+    "tw" : "twitter.com",
+    "yt" : "youtube.com",
+    "az" : "amazon.com"
+}
+
+const blockedSitesKey = "StudyBuddyBlockedSites"
+const statusKey = "StudyBuddyActive"
+
 // check if the user clicked on the "add" Image button
 addPicButton.addEventListener("click",function() {
     if (otherField.classList.contains("show")){
@@ -20,6 +34,30 @@ addButton.addEventListener("click", function(e){
     hide();
 })
 
+studyButton.addEventListener("click", async (e) => {
+    const storageObject = await browser.storage.local.get(statusKey);
+    const isActive = storageObject[statusKey]
+    if (isActive === false) {
+        browser.storage.local.set({[statusKey]:true});
+        studyButton.innerText = "Stop!";
+    } else {
+        browser.storage.local.set({[statusKey]:false});
+        studyButton.innerText = "Study!";
+    }
+});
+
+for (elem of socialInputs) {
+    elem.addEventListener("click", (e) => {
+        const site = siteMappings[e.target.value];
+        const checkboxState = e.target.checked;
+        if (checkboxState === true) {
+            addBlockedSite(site);
+        } else {
+            removeBlockedSite(site);
+        }
+    });
+}
+
 // hides the "new site" input field
 function hide() {
     otherField.classList.remove("show");
@@ -33,3 +71,50 @@ function show() {
     otherField.classList.add("show");
     addPicButton.src = "../socials/minus.png";
 }
+
+const addBlockedSite = async (site) => {
+    const storageObject = await browser.storage.local.get(blockedSitesKey)
+    const currentSites = storageObject[blockedSitesKey]
+    currentSites.add(site);
+    browser.storage.local.set({[blockedSitesKey]:currentSites});
+}
+
+const removeBlockedSite = async (site) => {
+    const storageObject = await browser.storage.local.get(blockedSitesKey);
+    const currentSites = storageObject[blockedSitesKey]
+    currentSites.delete(site);
+    browser.storage.local.set({[blockedSitesKey]:currentSites});
+}
+
+const init = async () => {
+    const storageObject = await browser.storage.local.get([blockedSitesKey, statusKey]);
+    if (blockedSitesKey in storageObject) {
+        blockedSites = storageObject[blockedSitesKey]
+        for (elem of socialInputs) {
+            if (blockedSites.has(siteMappings[elem.value])) {
+                elem.checked = true;
+            }
+        }
+    } else {
+        browser.storage.local.set({[blockedSitesKey]: new Set()});
+    }
+
+    if (statusKey in storageObject) {
+        
+    } else {
+        browser.storage.local.set({[statusKey]:false});
+    }
+}
+
+browser.webRequest.onBeforeRequest.addListener(
+    (req) => {
+        console.log(req)
+        return { 
+            redirectUrl: "google.com"
+        };
+    },
+    { urls: ["<all_urls>"] },
+    ['blocking']
+);
+
+document.addEventListener('DOMContentLoaded', init);
